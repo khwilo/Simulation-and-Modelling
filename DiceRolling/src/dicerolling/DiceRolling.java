ae3342842f06e98f7c94a93081c1cefb98f5991c
Khwilo <kwhyzzy@gmail.com>
2016-02-01 04:46:23 +0300

    use SecureRandom number generator and add more comments for clarity

--- DiceRolling/src/dicerolling/DiceRolling.java
+++ DiceRolling/src/dicerolling/DiceRolling.java
@@ -9,7 +9,7 @@ package dicerolling;
  *
  * @author Khwilo
  */
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class DiceRolling {
 
@@ -17,32 +17,40 @@ public class DiceRolling {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Random randomNum = new Random();
-        int[] array = new int[7];
+        SecureRandom randomNum = new SecureRandom();
+        /*
+         * Array of frequency counters.
+         * Occurences of each side of the die.
+         * Note: frequency[0] is ignored.
+        */
+        int[] frequency = new int[7];
        
         // Roll the die 1000 times
         int rolls;
         for (rolls = 1; rolls <= 1000; rolls++) {
-            ++array[ 1 + randomNum.nextInt(6)];
+            /*
+             * Determine which frequency element to increment at each iteration of the loop
+            */
+            ++frequency[ 1 + randomNum.nextInt(6)];
             
         }
         
         System.out.printf("%5s%12s%12s\n", "Face", "Frequency", "Percentage");
         System.out.println("-----------------------------");
-        double totalPerc = 0;
-        for (int faces = 1; faces < array.length ; faces++) {
-            double percentage = (double) (array[faces]) / 10;
+        double totalPercentage = 0;
+        for (int face = 1; face < frequency.length ; face++) {
+            double percentage = (double) (frequency[face]) / 10;
             String perc = String.format("%.1f", percentage);
-            totalPerc += percentage;
-            System.out.printf("%4d%10d%11s\n", faces, array[faces], perc);
+            totalPercentage += percentage;
+            System.out.printf("%4d%10d%11s\n", face, frequency[face], perc);
         }
         
         int frequencies = 0;
-        for (int num : array) {
+        for (int num : frequency) {
             frequencies += num;
         }
         
-        String percentage = String.format("%.0f", totalPerc);
+        String percentage = String.format("%.0f", totalPercentage);
         System.out.println("-----------------------------");
         System.out.printf("%5s%9d%9s \n:", "TOTAL ", frequencies, percentage);
     }
